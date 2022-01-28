5d09d836960a4f9a67881fe6bced455509c6ec42
cboscariol <caamila.boscariol@gmail.com>
2022-01-28 16:25:05 -0300

    about component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from './Containers/Header';
 import Main from './Containers/Main';
 import SubMain from './Containers/SubMain';
 import Banner from './Containers/Banner'
+import About from './Components/About';
+import about01 from './assets/image/about-01.png'
+import about02 from './assets/image/about-02.png'
 
 function App() {
   return (
@@ -11,6 +14,17 @@ function App() {
       <Main />
       <SubMain />
       <Banner />
+      <About
+        title='Vivamus at vestibulum tellus. Phasellus sit amet.'
+        text='Aenean pretium varius posuere. Aenean porttitor elementum tristique. Etiam eget metus lobortis, 
+        ultricies nisl vitae, tempus felis. Proin lacinia justo non elit semper consequat.'
+        image={about01} />
+      <About
+        title='Ut vel laoreet magna, tempor finibus augue.'
+        text='Aenean pretium varius posuere. Aenean porttitor elementum tristique. Etiam eget metus lobortis, 
+      ultricies nisl vitae, tempus felis. Proin lacinia justo non elit semper consequat.'
+        image={about02}
+        className="flex-reverse" />
     </div>
   );
 }
---
+++ src/Components/About/index.js
@@ -0,0 +1,18 @@
+import './style.css'
+import Container from '../Container'
+
+function About(props) {
+  return (
+    <div className='about'>
+      <Container className={`about-container ${props.className}`}>
+        <div className='about-text-box'>
+          <h3>{props.title}</h3>
+          <p>{props.text}</p>
+        </div>
+        <img src={props.image} alt="imagem" />
+      </Container>
+    </div>
+  );
+}
+
+export default About;
